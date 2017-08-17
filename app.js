90d0f6c2e67b203f7539038e0f7ad8bda8710b5f
Michiel van Roon <michiel@michielvanroon.nl>
2017-08-17 14:51:55 +0200

    fixed code

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
-angular.module('testApp', [
-  'testApp.controllers'
+angular.module('app', [
+  'app.controllers'
 ]);
+
+angular.module('app.controllers', []).
+controller('controller', ['$scope', function($scope) {
+
+  $scope.isSignedIn = false
+
+	window.updateSigninStatus = function(isSignedIn) {
+    $scope.isSignedIn = isSignedIn
+    $scope.$apply();
+
+	  if (isSignedIn) {
+      gapi.client.sheets.spreadsheets.values.get({
+  	    spreadsheetId: '1auacuh7Jsb9sKQiMwW5MfRTMyflxU_rJLEstWyl5-I0',
+  	    range: 'A:B',
+  	  }).then(function(response) {
+  			$scope.sheet = response.result.values
+        $scope.$apply();
+  	  }, function(response) {
+  			$scope.error = response.result.error.message
+        $scope.$apply();
+  	  });
+    }
+	}
+
+	//Sign in the user upon button click.
+	$scope.handleAuthClick = function(event) {
+	  gapi.auth2.getAuthInstance().signIn();
+	}
+
+	//Sign out the user upon button click.
+	$scope.handleSignoutClick = function(event) {
+	  gapi.auth2.getAuthInstance().signOut();
+	}
+}])
